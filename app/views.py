e65ca2a5181bf4280d87614c9600df22bbf678ab
manoj6281 <manojkumaraaa10@gmail.com>
2023-12-08 17:16:56 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def manoj(request):
+    return render(request,'manoj.html')
+
+
+
+def srujana(request):
+    return render(request,'srujana.html')
+
+
+
+def manoj2(request):
+    return render(request,'manoj2.html')
